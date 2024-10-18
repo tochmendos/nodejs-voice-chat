c5d448ed5278c5b95e0068f443cf505ea54e0596
tach <togmetk@gmail.com>
2024-10-19 01:12:52 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+    cors: {
+      origin: '*',
+    }
+  });
 
 //To holding users information 
 const socketsStatus = {};
